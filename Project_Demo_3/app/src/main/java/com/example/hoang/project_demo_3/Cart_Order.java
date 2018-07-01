3d605241db34a54122f02ff9545d559bea4de5a5
longstl <jonnykhanh1102@gmail.com>
2018-07-01 22:12:56 +0700

    Add Cart Layout

---
+++ Project_Demo_3/app/src/main/java/com/example/hoang/project_demo_3/Cart_Order.java
@@ -0,0 +1,13 @@
+package com.example.hoang.project_demo_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cart_Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_order);
+    }
+}
